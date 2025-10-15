b0b221e3334bc02c766ee63655606fe245c1fd13
Vadikari Vinuthna <150324126+Vinuthnavadikari@users.noreply.github.com>
2025-10-15 16:39:12 +0530

    Time: 0 ms (100%), Space: 40.9 MB (10.76%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.java
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -1,5 +1,12 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        return Integer.bitCount(x^y);
+        // return Integer.bitCount(x^y);
+        int xor=x^y;
+        int count=0;
+        while(xor!=0){
+            count+=xor&1;
+            xor>>=1;
+        }
+        return count;
     }
 }
