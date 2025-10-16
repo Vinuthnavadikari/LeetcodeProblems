fa3dfb90ace3f3657fe2cab8ba502ecfafe60d78
Vadikari Vinuthna <150324126+Vinuthnavadikari@users.noreply.github.com>
2025-10-16 17:58:56 +0530

    Time: 23 ms (29.26%), Space: 46.1 MB (21.06%) - LeetHub

---
+++ 2881-split-strings-by-separator/2881-split-strings-by-separator.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.regex.Pattern;
+
+class Solution {
+    public List<String> splitWordsBySeparator(List<String> words, char separator) {
+        List<String> list = new ArrayList<>();
+
+        for (String word : words) {
+          
+            String[] parts = word.split(Pattern.quote(String.valueOf(separator)));
+            for (String part : parts) {
+                if (!part.isEmpty()) {
+                    list.add(part);
+                }
+            }
+        }
+
+        return list;
+    }
+}
