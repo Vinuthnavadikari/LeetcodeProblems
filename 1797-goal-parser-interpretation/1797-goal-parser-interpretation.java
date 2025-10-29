321ce9d0ca650d441b318e62e9043b1fbd9314b1
Vadikari Vinuthna <150324126+Vinuthnavadikari@users.noreply.github.com>
2025-10-29 10:47:48 +0530

    Time: 1 ms (62.17%), Space: 41.6 MB (62.88%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.java
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.java
@@ -1,25 +1,28 @@
 class Solution {
     public String interpret(String command) {
-        StringBuilder s=new StringBuilder();
-        int n=command.length();
-        for(int i=0;i<n;i++){
-            char curr=command.charAt(i);
-            if(curr=='G'){
-                s.append('G');
-            }
-            else if(curr == '('){
-                if(i+1<n && command.charAt(i+1) == ')'){
-                    s.append("o");
-                    i++;
-                }
-                else if(i+3<n && command.substring(i,i+4).equals("(al)")){
-                    s.append("al");
-                    i+=3;
+        // StringBuilder s=new StringBuilder();
+        // int n=command.length();
+        // for(int i=0;i<n;i++){
+        //     char curr=command.charAt(i);
+        //     if(curr=='G'){
+        //         s.append('G');
+        //     }
+        //     else if(curr == '('){
+        //         if(i+1<n && command.charAt(i+1) == ')'){
+        //             s.append("o");
+        //             i++;
+        //         }
+        //         else if(i+3<n && command.substring(i,i+4).equals("(al)")){
+        //             s.append("al");
+        //             i+=3;
 
-                }
-            }
-        }
-        return s.toString();
+        //         }
+        //     }
+        // }
+        // return s.toString();
+        String s=command.replace("()","o");
+        String r=s.replace("(al)","al");
+        return r;
         
     }
 }
