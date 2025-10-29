135397ffc8fdd56de13a2e897880cdf853009ddc
Vadikari Vinuthna <150324126+Vinuthnavadikari@users.noreply.github.com>
2025-10-29 10:45:26 +0530

    Time: 0 ms (100%), Space: 41.5 MB (90.06%) - LeetHub

---
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String interpret(String command) {
+        StringBuilder s=new StringBuilder();
+        int n=command.length();
+        for(int i=0;i<n;i++){
+            char curr=command.charAt(i);
+            if(curr=='G'){
+                s.append('G');
+            }
+            else if(curr == '('){
+                if(i+1<n && command.charAt(i+1) == ')'){
+                    s.append("o");
+                    i++;
+                }
+                else if(i+3<n && command.substring(i,i+4).equals("(al)")){
+                    s.append("al");
+                    i+=3;
+
+                }
+            }
+        }
+        return s.toString();
+        
+    }
+}
