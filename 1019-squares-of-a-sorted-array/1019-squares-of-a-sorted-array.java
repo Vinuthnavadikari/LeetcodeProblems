49b64553b82cd6b8284294e995c022a97d26d0f0
Vadikari Vinuthna <150324126+Vinuthnavadikari@users.noreply.github.com>
2025-10-30 19:09:09 +0530

    Time: 7 ms (44.95%), Space: 47.8 MB (8.2%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -1,14 +1,12 @@
 import java.util.*;
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        List<Integer>list=new ArrayList<>();
+       
         for(int i=0;i<nums.length;i++){
-            list.add((int)Math.pow(nums[i],2));
+           nums[i]=((int)Math.pow(nums[i],2));
 
         }
-        for(int i=0;i<nums.length;i++){
-            nums[i]=list.get(i);
-        }
+       
         Arrays.sort(nums);
         return nums;
 
