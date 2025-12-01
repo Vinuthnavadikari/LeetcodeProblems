8270ce35b1ad065e8b3d8aadd765d13d181d0161
Vadikari Vinuthna <150324126+Vinuthnavadikari@users.noreply.github.com>
2025-12-01 20:57:34 +0530

    Time: 6 ms (35.12%), Space: 43.9 MB (24.06%) - LeetHub

--- 3280-convert-date-to-binary/3280-convert-date-to-binary.java
+++ 3280-convert-date-to-binary/3280-convert-date-to-binary.java
@@ -1,12 +1,14 @@
 class Solution {
     public String convertDateToBinary(String date) {
-        String []parts=date.split("-");
-        int year=Integer.parseInt(parts[0]);
-        int mon=Integer.parseInt(parts[1]);
-        int da=Integer.parseInt(parts[2]);
-        String y=Integer.toBinaryString(year);
-        String m=Integer.toBinaryString(mon);
-        String d=Integer.toBinaryString(da);
-        return y+"-"+m+"-"+d;
+        String[] parts = date.split("-");
+        int year = Integer.parseInt(parts[0]);
+        int month = Integer.parseInt(parts[1]);
+        int day = Integer.parseInt(parts[2]);
+        
+        String y = Integer.toBinaryString(year);
+        String m = Integer.toBinaryString(month);
+        String d = Integer.toBinaryString(day);
+        
+        return y + "-" + m + "-" + d;
     }
 }
