dbd88272703cb354f605eb63bed6439ad2fbcf6b
Vadikari Vinuthna <150324126+Vinuthnavadikari@users.noreply.github.com>
2026-01-02 12:28:14 +0530

    Time: 3 ms (76.78%), Space: 43.7 MB (51.69%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -1,17 +1,34 @@
 import java.math.BigInteger;
 class Solution {
     public String multiply(String num1, String num2) {
-        // if (num1.equals("0") || num2.equals("0"))
-        //     return "0";
+       if (num1.equals("0") || num2.equals("0"))
+            return "0";
+        int n=num1.length();
+        int m=num2.length();
+        int res[]=new int[n+m];
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                int d1=num1.charAt(i)-'0';
+                int d2=num2.charAt(j)-'0';
+                int mul=d1*d2;
+                int sum=mul+res[i+j+1];
+                res[i+j+1]=sum%10;
+                res[i+j]+=sum/10;
+
+            }
+           
+
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int num : res) {
+            if (!(sb.length() == 0 && num == 0)) {
+                sb.append(num);
+            }
+        }
+
+        return sb.toString();
 
 
-        // int n1=Integer.parseInt(num1);
-        // int n2=Integer.parseInt(num2);
-        // int mul=n1*n2;
-        // return Integer.toString(mul);
-        BigInteger a=new BigInteger(num1);
-        BigInteger b=new BigInteger(num2);
-        return a.multiply(b).toString();
+
     }
-
 }
