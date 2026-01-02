68ea614b3506cb475984938025a6a491ee7c8880
Vadikari Vinuthna <150324126+Vinuthnavadikari@users.noreply.github.com>
2026-01-02 10:23:56 +0530

    Time: 1 ms (65.88%), Space: 47.1 MB (89.44%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -1,20 +1,20 @@
 class Solution {
     public int repeatedNTimes(int[] nums) {
-        // int frq[]=new int[10];
-        // for(int num:nums){
-        //     frq[num]++;
-        //     if(frq[num]>1){
-        //         return num;
-        //     }
-        // }
-        // return 0;
-        HashSet<Integer> set=new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            if(!set.add(nums[i])){
-                return nums[i];
+        int frq[]=new int[10001];
+        for(int num:nums){
+            frq[num]++;
+            if(frq[num]>1){
+                return num;
             }
         }
         return -1;
+        // HashSet<Integer> set=new HashSet<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(!set.add(nums[i])){
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
         
     }
 }
