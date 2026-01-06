9f324c606c610c16478cb6bb247bb380e94e9e04
Vadikari Vinuthna <150324126+Vinuthnavadikari@users.noreply.github.com>
2026-01-06 17:36:25 +0530

    Time: 53 ms (91.23%), Space: 105.6 MB (60.58%) - LeetHub

--- 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.java
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.java
@@ -23,23 +23,43 @@
 //         return s.size();
 //     }
 // }
+// class Solution {
+//     public int countDistinctIntegers(int[] nums) {
+//         HashSet<Integer> set = new HashSet<>();
+
+//         for (int num : nums) {
+//             set.add(num);
+
+//             int rev = 0;
+//             int temp = num; 
+//             while (temp != 0) {
+//                 rev = rev * 10 + (temp % 10);
+//                 temp /= 10;
+//             }
+
+//             set.add(rev);
+//         }
+
+//         return set.size();
+//     }
+// }
 class Solution {
     public int countDistinctIntegers(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-
-        for (int num : nums) {
-            set.add(num);
-
-            int rev = 0;
-            int temp = num; 
-            while (temp != 0) {
-                rev = rev * 10 + (temp % 10);
-                temp /= 10;
-            }
-
-            set.add(rev);
-        }
-
+        HashSet<Integer> set=new HashSet<>();
+        for(int a:nums){
+            int re=reverse(a);
+            set.add(a);
+            set.add(re);
+        } 
         return set.size();
+    }
+    private int reverse(int n){
+        int rev=0;
+        while(n!=0){
+            int digit=n%10;
+            rev=rev*10+digit;
+            n=n/10;
+        }
+        return rev;
     }
 }
