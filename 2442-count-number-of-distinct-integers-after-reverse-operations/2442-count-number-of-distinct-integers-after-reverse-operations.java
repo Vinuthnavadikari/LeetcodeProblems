e5690e258a726ecf4c7a09613281db9f48847a5f
Vadikari Vinuthna <150324126+Vinuthnavadikari@users.noreply.github.com>
2026-01-06 17:35:36 +0530

    Time: 59 ms (50.96%), Space: 105.4 MB (72.84%) - LeetHub

--- 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.java
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.java
@@ -1,25 +1,45 @@
+// class Solution {
+//     public int countDistinctIntegers(int[] nums) {
+//         ArrayList<Integer> a=new ArrayList<>();
+//         for(int i=0;i<nums.length;i++){
+//             a.add(nums[i]);
+//         }
+//         for(int i=0;i<nums.length;i++){
+//             int rev=0;
+//             while(nums[i]!=0){
+//                 int digit=nums[i]%10;
+//                 rev=rev*10+digit;
+//                 nums[i]/=10;
+//             }
+//             a.add(rev);
+//         }
+        
+//         HashSet<Integer> s=new HashSet<>();
+//         for(int arr:a){
+//             if(!s.contains(arr)){
+//                 s.add(arr);
+//             }
+//         }
+//         return s.size();
+//     }
+// }
 class Solution {
     public int countDistinctIntegers(int[] nums) {
-        ArrayList<Integer> a=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            a.add(nums[i]);
-        }
-        for(int i=0;i<nums.length;i++){
-            int rev=0;
-            while(nums[i]!=0){
-                int digit=nums[i]%10;
-                rev=rev*10+digit;
-                nums[i]/=10;
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(num);
+
+            int rev = 0;
+            int temp = num; 
+            while (temp != 0) {
+                rev = rev * 10 + (temp % 10);
+                temp /= 10;
             }
-            a.add(rev);
+
+            set.add(rev);
         }
-        
-        HashSet<Integer> s=new HashSet<>();
-        for(int arr:a){
-            if(!s.contains(arr)){
-                s.add(arr);
-            }
-        }
-        return s.size();
+
+        return set.size();
     }
 }
