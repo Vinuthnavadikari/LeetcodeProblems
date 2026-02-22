76698ce04d6bb5a5d9b236ceb76aaa21efc483ea
Vadikari Vinuthna <150324126+Vinuthnavadikari@users.noreply.github.com>
2026-02-22 22:15:35 +0530

    Time: 1 ms (99.05%), Space: 44.4 MB (31.87%) - LeetHub

--- 3184-count-pairs-that-form-a-complete-day-i/3184-count-pairs-that-form-a-complete-day-i.java
+++ 3184-count-pairs-that-form-a-complete-day-i/3184-count-pairs-that-form-a-complete-day-i.java
@@ -1,15 +1,31 @@
+// class Solution {
+//     public int countCompleteDayPairs(int[] hours) {
+//         int c=0;
+//         for(int i=0;i<hours.length;i++){
+//             for(int j=i+1;j<hours.length;j++){
+//                 if((hours[i]+hours[j])%24==0){
+//                     c++;
+
+//                 }
+
+//             }
+//         }
+//         return c;
+//     }
+// }
 class Solution {
     public int countCompleteDayPairs(int[] hours) {
-        int c=0;
-        for(int i=0;i<hours.length;i++){
-            for(int j=i+1;j<hours.length;j++){
-                if((hours[i]+hours[j])%24==0){
-                    c++;
+        int[] freq = new int[24];
+        int count = 0;
 
-                }
+        for(int h : hours){
+            int rem = h % 24;
+            int complement = (24 - rem) % 24;
 
-            }
+            count += freq[complement];
+            freq[rem]++;
         }
-        return c;
+
+        return count;
     }
 }
