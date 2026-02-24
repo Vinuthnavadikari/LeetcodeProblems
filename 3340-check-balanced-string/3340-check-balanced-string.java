d2a42f2e732869705688f572024ce6938d9646c5
Vadikari Vinuthna <150324126+Vinuthnavadikari@users.noreply.github.com>
2026-02-24 12:59:11 +0530

    Time: 1 ms (98.38%), Space: 43.5 MB (66%) - LeetHub

--- 3340-check-balanced-string/3340-check-balanced-string.java
+++ 3340-check-balanced-string/3340-check-balanced-string.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean isBalanced(String num) {
         int sume=0;
-        int sumo=0;
+     
         for(int i=0;i<num.length();i++){
           if(i%2==0){
             sume+=num.charAt(i)-'0';
           }
           else{
-            sumo+=num.charAt(i)-'0';
+            sume-=num.charAt(i)-'0';
           }
         }
-        return sume==sumo;
+        return sume==0;
         
         
     }
