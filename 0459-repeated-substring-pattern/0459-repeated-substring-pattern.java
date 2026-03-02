bd05eae5b309ab1d268815087b3d6ecce243b2a3
Vadikari Vinuthna <150324126+Vinuthnavadikari@users.noreply.github.com>
2026-03-02 21:42:53 +0530

    Time: 75 ms (49.72%), Space: 47 MB (52.61%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,41 +1,48 @@
+// class Solution {
+//     public boolean repeatedSubstringPattern(String s) {
+
+//         int n = s.length();
+
+//         for(int len = 1; len <= n/2; len++){
+
+//             if(n % len != 0) continue;
+
+//             String pattern = s.substring(0, len);
+//             Stack<Character> st = new Stack<>();
+
+//             for(char c : s.toCharArray()){
+//                 st.push(c);
+
+//                 if(st.size() >= len){
+//                     boolean match = true;
+
+//                     for(int i = 0; i < len; i++){
+//                         if(st.get(st.size() - len + i) != pattern.charAt(i)){
+//                             match = false;
+//                             break;
+//                         }
+//                     }
+
+//                     if(match){
+//                         for(int i = 0; i < len; i++){
+//                             st.pop();
+//                         }
+//                     }
+//                 }
+//             }
+
+//             if(st.isEmpty()){
+//                 return true;
+//             }
+//         }
+
+//         return false;
+//     }
+// }
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-
-        int n = s.length();
-
-        for(int len = 1; len <= n/2; len++){
-
-            if(n % len != 0) continue;
-
-            String pattern = s.substring(0, len);
-            Stack<Character> st = new Stack<>();
-
-            for(char c : s.toCharArray()){
-                st.push(c);
-
-                if(st.size() >= len){
-                    boolean match = true;
-
-                    for(int i = 0; i < len; i++){
-                        if(st.get(st.size() - len + i) != pattern.charAt(i)){
-                            match = false;
-                            break;
-                        }
-                    }
-
-                    if(match){
-                        for(int i = 0; i < len; i++){
-                            st.pop();
-                        }
-                    }
-                }
-            }
-
-            if(st.isEmpty()){
-                return true;
-            }
-        }
-
-        return false;
+        String doubled = s + s;
+        String check = doubled.substring(1, doubled.length() - 1);
+        return check.contains(s);
     }
 }
