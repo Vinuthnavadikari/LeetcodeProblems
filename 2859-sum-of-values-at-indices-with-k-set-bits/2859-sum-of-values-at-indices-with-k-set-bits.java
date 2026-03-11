2937aa99a1ba89ba4fc10eac9f33b2e3490a0dea
Vadikari Vinuthna <150324126+Vinuthnavadikari@users.noreply.github.com>
2026-03-11 15:39:18 +0530

    Time: 2 ms (28.7%), Space: 46.4 MB (77.22%) - LeetHub

--- 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.java
+++ 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.java
@@ -3,7 +3,17 @@ class Solution {
         int count=0;
         for(int i=0;i<nums.size();i++)
         {
-            if(Integer.bitCount(i)==k) count+=nums.get(i);
+            if(bitCount(i)==k) count+=nums.get(i);
+        }
+        return count;
+    }
+    static int bitCount(int i)
+    {
+        int count=0;
+        while(i>0)
+        {
+            if((i&1)==1)count++;
+            i=i/2;
         }
         return count;
     }
