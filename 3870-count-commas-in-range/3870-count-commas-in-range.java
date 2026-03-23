91048fb4c4beb1713dbfc61979822af71f146c17
Vadikari Vinuthna <150324126+Vinuthnavadikari@users.noreply.github.com>
2026-03-23 09:45:06 +0530

    Time: 1 ms (87.49%), Space: 42.5 MB (86.64%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,9 +1,8 @@
 class Solution {
     public int countCommas(int n) {
-   String a=String.valueOf(n);
-   int digit=a.length();
+  int a=(int)Math.log10(n)+1;
        
-        if(digit<4){
+        if(a<4){
             return 0;
         }
         return n-1000+1;
