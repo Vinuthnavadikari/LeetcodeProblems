9c67c841867a1a5168806f324bce9aa7e48dfcbc
Vadikari Vinuthna <150324126+Vinuthnavadikari@users.noreply.github.com>
2026-03-31 17:21:09 +0530

    Time: 6 ms (84.31%), Space: 47.2 MB (33.03%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,26 +1,13 @@
 class Solution {
     public int firstUniqChar(String s) {
-        HashSet<Character>seenOnce=new HashSet<>();
-        HashSet<Character>seenMore=new HashSet<>();
-        for(char c:s.toCharArray()){
-            if(seenMore.contains(c)){
-                continue;
-            }
-            if(seenOnce.contains(c)){
-                seenOnce.remove(c);
-                seenMore.add(c);
-                
-            }
-            else{
-                seenOnce.add(c);
-            }
-
+        int[] count = new int[26];
+        for(int i=0; i<s.length(); i++){
+            count[s.charAt(i)- 'a']++;
         }
-        for(int i=0;i<s.length();i++){
-            if(seenOnce.contains(s.charAt(i))){
+        for(int i=0; i<s.length(); i++){
+            if(count[s.charAt(i) - 'a']==1){
                 return i;
             }
-
         }
         return -1;
     }
