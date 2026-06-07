ca4a6f2327ec6b3ea319a4388e9856c20cb6a002
Vadikari Vinuthna <150324126+Vinuthnavadikari@users.noreply.github.com>
2026-06-07 15:21:16 +0530

    Time: 0 ms (100%), Space: 44.4 MB (81.95%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,39 +1,38 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int idx=-1;
-        for(int i=nums.length-2;i>=0;i--){
+        int n=nums.length;
+        for(int i=n-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
                 idx=i;
                 break;
-
             }
+
         }
         if(idx==-1){
-            reverse(nums,0,nums.length-1);
+            reverse(nums,0,n-1);
             return;
         }
-        for(int i=nums.length-1;i>idx;i--){
+        for(int i=n-1;i>idx;i--){
             if(nums[i]>nums[idx]){
                 swap(nums,i,idx);
                 break;
             }
 
         }
-        reverse(nums,idx+1,nums.length-1);
+        reverse(nums,idx+1,n-1);
 
     }
-    private void reverse(int []nums,int st,int end){
+    private void reverse(int nums[],int st,int end){
         while(st<end){
             swap(nums,st,end);
             st++;
             end--;
         }
     }
-    private void swap(int nums[],int i,int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
+    private void swap(int nums[],int st,int end){
+        int temp=nums[st];
+        nums[st]=nums[end];
+        nums[end]=temp;
     }
-
-    
 }
