ce8db9e7d9c722de5fec0838866bdc4c48c393ec
Vadikari Vinuthna <150324126+Vinuthnavadikari@users.noreply.github.com>
2026-06-30 15:52:08 +0530

    Time: 0 ms (100%), Space: 48.1 MB (50.36%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,39 +1,50 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int l=lb(nums,nums.length,target);
-        if(l==nums.length || nums[l]!=target)return new int[]{-1,-1};
-        return new int[]{l,up(nums,nums.length,target)-1};
+        int first=f(nums,nums.length,target);
+        if(first==-1){
+            return new int[]{-1,-1};
+        }
+        return new int[]{first,l(nums,nums.length,target)};
+      
         
     }
-    private int lb(int nums[],int n,int x){
+    private int f(int[]nums,int n,int x){
         int l=0,h=n-1;
-        int ans=n;
+        int first=-1;
         while(l<=h){
             int mid=(l+h)/2;
-            if(nums[mid]>=x){
-                ans=mid;
+            if(nums[mid]==x){
+                first=mid;
                 h=mid-1;
             }
-            else{
+            else if(nums[mid]<x){
                 l=mid+1;
             }
-        }
-        return ans;
-    }
-    private int up(int nums[],int n,int x){
-        int l=0,h=n-1;
-        int ans=n;
-        while(l<=h){
-            int mid=(l+h)/2;
-            if(nums[mid]>x){
-                ans=mid;
+            else{
                 h=mid-1;
-            }
-            else{
-                l=mid+1;
             }
 
         }
-        return ans;
+        return first;
+
     }
+    private int l(int nums[],int n,int x){
+        int l=0,h=n-1;
+        int last=-1;
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(nums[mid]==x){
+                last=mid;
+                l=mid+1;
+            }
+            else if(nums[mid]<x){
+                l=mid+1;
+            }
+            else{
+                h=mid-1;
+            }
+        }
+        return last;
+    }
+   
 }
