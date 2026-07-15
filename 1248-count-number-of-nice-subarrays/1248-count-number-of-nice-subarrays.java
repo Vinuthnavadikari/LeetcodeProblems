1d518c87b3f98413a83940f9f00d62db79ebcf82
Vadikari Vinuthna <150324126+Vinuthnavadikari@users.noreply.github.com>
2026-07-15 14:20:49 +0530

    Time: 44 ms (15.73%), Space: 56.4 MB (87.35%) - LeetHub

---
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        Map<Integer,Integer>map=new HashMap<>();
+        map.put(0,1);
+        int oddCount=0;
+        int ans=0;
+        for(int num:nums){
+            if(num%2==1){
+                oddCount++;
+            }
+            if(map.containsKey(oddCount-k)){
+                ans+=map.get(oddCount-k);
+            }
+            map.put(oddCount,map.getOrDefault(oddCount,0)+1);
+        }
+        return ans;
+        
+        
+    }
+}
